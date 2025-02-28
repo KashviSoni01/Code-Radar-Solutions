483bcd2e93c879975dfe23dd62e0a2b1e0daa1eb
KashviSoni01 <sonikashvi15@gmail.com>
2025-02-28 13:09:57 +0530

    Solution to Print an Alternating Numeric Triangle

--- Print an Alternating Numeric Triangle@@67877e1818d53eb724f435cd/code.c
+++ Print an Alternating Numeric Triangle@@67877e1818d53eb724f435cd/code.c
@@ -1,10 +1,25 @@
 #include <stdio.h>
-
-char* welcome() {
-    return "Welcome to Code Radar!";
-}
-
 int main() {
-    printf("%s", welcome());
-    return 0;
+    int i,j,n;
+    scanf("%d", &n);
+    for (i=1;i<=n;i++) {
+        for(j=1;j<=i;j++) {
+            if (i%2==0) {
+                if (j%2==0) {
+                    printf("1 ");
+                }
+                else {
+                    printf("0 ");
+                }
+            }
+            else {
+                if (j%2==0) {
+                    printf("0 ");
+                }
+                else {
+                    printf("1 ");
+                }
+            }
+        }
+    }
 }
